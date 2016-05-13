8fcec9d854f93aacece63365660d900ead087aed
bapboy18 <duongnv.dhbk@gmail.com>
2016-05-13 14:36:35 +0700

    110-image_url

--- app_xemay/spec/fabricators/image_fabricator.rb
+++ app_xemay/spec/fabricators/image_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:image) do
-  image {Faker::Avatar.image}
+  image {File.open(File.join(Rails.root, "app", "assets", "images", "motorcycle.png"))}
   caption {Faker::Lorem.sentence(3, true, 1)}
 end
