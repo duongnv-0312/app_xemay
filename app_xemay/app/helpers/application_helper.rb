3923a44bde8e67f8e1da83bbda202a3c4950ab95
bapboy18 <duongnv.dhbk@gmail.com>
2016-05-20 19:55:03 +0700

    83-review-show

--- app_xemay/app/helpers/application_helper.rb
+++ app_xemay/app/helpers/application_helper.rb
@@ -18,4 +18,24 @@ module ApplicationHelper
     href = html_options[:href] || '#'
     content_tag :a, name, html_options.merge(href: href, onclick: onclick)
   end
+
+  def created_mins time
+    ((Time.now - time)/ 60).round
+  end
+
+  def created_hours time
+    ((Time.now - time)/ 1.hour).round
+  end
+
+  def created_days time
+    ((Time.now - time)/ 1.day).round
+  end
+
+  def created_months time
+    ((Time.now - time)/ 1.month).round
+  end
+
+  def created_years time
+    ((Time.now - time)/ 1.year).round
+  end
 end
